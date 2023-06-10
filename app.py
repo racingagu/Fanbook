96c173c7843adecdf7c853008f5f7225b0cd0a88
racingagu <racingagus99@gmail.com>
2023-06-10 08:13:02 +0700

    modify

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ dotenv_path = join(dirname(__file__),'.env')
 load_dotenv(dotenv_path)
 
 MONGODB_URI = os.environ.get('mongodb+srv://AgusSantoso:iCGihqUncwckUEaz@cluster0.c3fyjgp.mongodb.net/?retryWrites=true&w=majority')
-DB_NAME = os.environ.get('AgusSantoso')
+DB_NAME = os.environ.get('Cluster0')
 
 client = MongoClient('mongodb+srv://AgusSantoso:iCGihqUncwckUEaz@cluster0.c3fyjgp.mongodb.net/?retryWrites=true&w=majority')
 db = client.dbsparta
